4afab9b9a0b17d2378687f4b10154c73aff7af14
jonathanrian <jonathasryann@gmail.com>
2024-09-11 09:42:45 -0300

    Commit "Projeto usando typeScrip"

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Header } from './components/Header';
+import { Post, PostType } from './components/Post';
+
+import styles from './App.module.css';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+
+const post: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/95014039?v=4',
+      name: 'Jonathan Ryan',
+      role: 'Web develop'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu GitHub. É um projeto que fiz nos estudos diarios. O nome do projeto é PrimeFlix 🚀',},
+      { type: 'link', content: 'https://prime-five-sable.vercel.app/',},
+    ], 
+    publishedAt: new Date('2024-09-10 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/95014039?v=4',
+      name: 'Samyra Ellen',
+      role: 'Farmaceutica'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu GitHub. É um projeto que fiz nos estudos diarios. O nome do projeto é PrimeFlix 🚀',},
+      { type: 'link', content: 'https://prime-five-sable.vercel.app/',},
+    ], 
+    publishedAt: new Date('2024-09-15 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        
+        <Sidebar />
+
+        <main>
+          {post.map(post => {
+            return (
+              <Post 
+                post={post}
+              />
+            )
+          })}
+        </main>
+
+        <ToastContainer autoClose={3000} />
+      </div>
+    </div>
+  )
+}
+
+
